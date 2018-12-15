1131cc10458ba2a7d126d13a93218029680ed9c6
q283570872 <283570872@qq.com>
2018-12-15 20:04:24 +0800

    vuecli

---
+++ frontend/maker/src/components/myHeader.vue
@@ -0,0 +1,229 @@
+<template>
+    <div>
+        <header>
+            <div class="logo d-none d-md-block">
+            <img src="@/assets/img/header.jpg" alt="" class="w-100">
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" style="border: 4px solid #ff9966;border-radius: 5px">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-index" aria-controls="navbar-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <form class="form-inline my-1">
+                <div class="btn-group">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <a href="#" class="btn my-1 my-sm-0 text-white" style="background-color: #ff9966">Search</a>
+                </div>
+            </form>
+            <div class="collapse navbar-collapse justify-content-end" id="navbar-index">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-3">
+                    <router-link class="nav-link" to="/">首页</router-link>
+                </li>
+                <li class="nav-item mx-3" v-if="!this.hasLogged">
+                    <a class="nav-link" @click="loginDialogShow" style="cursor: pointer;">登录</a>
+                </li>
+                <li class="nav-item mx-3" v-if="this.hasLogged">
+                    <a class="nav-link" style="cursor: pointer;">尊贵的{{myUsername}},您好!</a>
+                </li>
+                <li class="nav-item mx-3" v-if="this.hasLogged">
+                    <a class="nav-link" style="cursor: pointer;" @click="exit">退出</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="/register">注册</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" style='cursor: pointer;'>购物车</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">创客中心</a>
+                </li>
+                </ul>
+            </div>	
+            </nav>
+        </header>
+        <div class="login" v-if="isLoginDialog">
+			<div class="login-box">
+				<button type="button" class="close m-2" aria-label="Close" @click="loginDialogHide">
+				  <span aria-hidden="true">&times;</span>
+				</button>
+				<div class="login-title px-4 py-4">密码登录</div>
+				<form>
+				  <div class="form-group row px-4 py-0">
+					<label for="username" class="col-2 col-form-label py-0 my-0"><div class="usernameIcon"><i class="iconfont ml-1">&#xe683;</i></div></label>
+					<div class="col-10">
+					  <input type="text" class="form-control justify-content-center" id="username" placeholder="会员名/邮箱号/手机号" v-model="myUsername">
+					</div>
+				  </div>
+				  <div class="form-group row px-4 py-2">
+					<label for="password" class="col-2 col-form-label py-0 my-0"><div class="passwordIcon"><i class="iconfont ml-1">&#xe620;</i></div></label>
+					<div class="col-10">
+					  <input type="password" class="form-control" id="password" placeholder="密码" v-model="myPassword">
+					</div>
+				  </div>
+				  <div class="form-group row px-4">
+					<div class="col-12">
+					  <button type="button" class="btn btn-primary w-100" @click="login">登录</button>
+					</div>
+				  </div>
+				</form>
+				<div class="forget px-4 py-2 float-left"><a href="#">忘记密码</a></div>
+				<div class="registe px-4 py-2 float-right"><a href="#">免费注册</a></div>
+			</div>
+		</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                myUsername:"",
+                myPassword:"",
+                users:"",
+                hasLogged:false,
+                isLoginDialog:false
+            }
+        },
+        methods: {
+            loginDialogShow(){
+                this.isLoginDialog=true;
+            },
+            loginDialogHide(){
+                this.isLoginDialog=false;
+            },
+            login(){
+                var isTrue=false
+                this.hasLogged=true;
+                this.isLoginDialog=false;
+                isTrue=true
+            },
+            exit(){
+                this.hasLogged=false
+                this.myUsername=""
+                this.myPassword=""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.logo-wraper{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.logo-main{
+	margin: 0 50px;
+	font-family: '华文行楷';
+	font-size: 30px
+}
+.store-name{
+	font-size: 20px;
+	font-weight:900;
+	font-family: '微软雅黑';
+}
+.login{
+	width: 350px;
+	height: 320px;
+	background-color: #fff;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	margin: auto;
+    border: 1px solid black;
+    z-index: 1000;
+}
+.login-box{
+	position: relative;
+}
+.login-close{
+	position: absolute;
+	top: 0;
+	right: 0;
+	font-size: 25px;
+	line-height: 25px;
+	cursor: pointer;
+}
+.login-title{
+	font-size: 25px;
+	font-family: '微软雅黑';
+}
+.usernameIcon{
+	background-color: #DDDDDD;
+	color: white;
+	width: 36px;
+	height: 36px;
+}
+.usernameIcon i{
+	font-size: 25px;
+	line-height: 36px;
+}
+.passwordIcon{
+	background-color: #DDDDDD;
+	color: white;
+	width: 36px;
+	height: 36px;
+}
+.passwordIcon i{
+	font-size: 27px;
+	line-height: 36px;
+}
+.flex{
+	display: flex;
+	justify-content: center;
+}
+.order-details-title{
+	font-size: 38px;
+	font-family: "黑体";
+	font-weight: bold;
+	color: #ff6600;
+}
+.logistics-information-title{
+	font-size: 20px;
+	font-family: "黑体";
+	font-weight: bold;
+}
+.bold{
+	font-size: 14px;
+	font-family: "黑体";
+	font-weight: bold;
+}
+.order-detail-title{
+	font-size: 16px;
+	font-family: "微软雅黑";
+	font-weight: bold;
+	background-color: #e0ebff;
+	color: #3C763D;
+	width: 100px;
+}
+.bold-24{
+	font-size: 24px;
+	font-family: "黑体";
+	font-weight: bold;
+}
+.font-10{
+	font-size: 10px;
+	font-family: '微软雅黑';
+}
+.font-12{
+	font-size: 12px;
+	color: #9a9a9a;
+	font-family: '微软雅黑';
+}
+.ellipsis{
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	overflow: hidden;
+}
+.addProduct{
+	background-color:#ff9966;
+	position:fixed;
+	left:50%;
+	top:50%;
+	transform:translate(-50%,-50%);
+	z-index:1000;
+	min-width: 300px;
+}
+</style>
---
+++ frontend/maker/src/views/store/store.vue
@@ -0,0 +1,116 @@
+<template>
+    <div>
+        <!-- 店铺名 -->
+		<section id="dianpuming">	
+			<div class="dianpumingcheng"><strong>阳光小铺</strong><img src="@/assets/img/erweima.png" style="margin-left:30px;width:50px"></div>
+		</section>
+		<div class="dianpumingcheng"></div>
+
+		<!-- 商品展示 -->
+        <div class="card">
+        <section id="shangpinzhanshi">
+			<div class="container" style="max-width: 1050px">	
+				<div class="row">
+				<div class="col-12 col-md-4 col-sm-12 mb-3 flex mx-0 px-1" v-for="(item,index) in shangpinzhanshi1" :key="index">
+					<div class="card" style="width:320px">
+						<img class="card-img-top m-auto py-2" :src="item.imageUrl" style="width:95%">
+								<div class="card-body">
+										<div class="name my-1"><router-link to="product" class="a1">{{item.name}}</router-link></div>
+										<div class="price my-2">{{item.price}}</div>									
+										<div class="browse">销量：{{item.browseNum}}</div>
+								</div>
+								</div>
+								</div>
+							</div>
+						</div>
+
+        </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                shangpinzhanshi1:[
+					{
+						name:"猫咪杯子情侣动物水杯可爱创意陶瓷马克杯咖啡生日礼物带杯盖勺子",
+						price:"¥20",
+						browseNum:"100",
+						imageUrl:"@/assets/img/list-1.jpg",
+						
+					},
+					{
+						name:"韩国卡通可爱猫咪蓝光苹果7plus手机iphonex/7/8/6s/6全包软壳",
+						price:"¥15",
+						browseNum:"80",
+						imageUrl:"@/assets/img/shoujike.jpg",
+						
+					},
+					{
+						name:"四川蒲江红心猕猴桃新鲜水果5斤包邮30个奇异果红阳泥猴桃",
+						price:"¥35",
+						browseNum:"60",
+						imageUrl:"@/assets/img/shuiguo.jpg",
+					
+					},
+					{
+						name:"有零有食脆脆每日坚果集混合160g营养代餐能量棒燕麦片零食",
+						price:"¥15",
+						browseNum:"50",
+						imageUrl:"@/assets/img/lingshi.jpg",
+						
+					},
+					{
+						name:"牛津布装棉被子的收纳袋超大搬家神器打包袋衣服物行李箱整理袋子",
+						price:"¥10",
+						browseNum:"35",
+						imageUrl:"@/assets/img/shounadai.jpg",
+						
+					},
+				]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+*{
+	margin: 0;
+	padding: 0;
+	list-style-type: none;
+	text-decoration: none;
+}
+a{
+	color: black;
+}
+.dianpumingcheng{
+	font-family: '华文彩云';
+	font-size:50px;
+	color:#ffcc66;
+	background-color:#ffffde;
+}
+
+.store-name {
+    font-family: '微软雅黑';
+    font-size: 15px;
+}
+
+.price{
+	font-size:30px;
+	color:red;
+}
+.browse{
+	color:red;
+}
+
+.card{
+	margin-top: 30px;
+}
+
+.goumaixuanxiang{
+	margin-left: 10px;
+	margin-bottom: 10px;
+}
+</style>
