5c79c6d68724b8a91fa399d078ab76f5263d922b
FengDK <814258761@qq.com>
2018-12-22 21:32:58 +0800

    Update maker.sql
    .

--- backend/maker.sql
+++ backend/maker.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 22/12/2018 21:17:59
+ Date: 22/12/2018 21:32:38
 */
 
 SET NAMES utf8mb4;
@@ -69,6 +69,24 @@ CREATE TABLE `comment`  (
   CONSTRAINT `fk[product_id]` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk[user_id]` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group_collections
+-- ----------------------------
+DROP TABLE IF EXISTS `group_collections`;
+CREATE TABLE `group_collections`  (
+  `user_id` int(55) NOT NULL COMMENT '用户ID',
+  `collect_group_id` int(55) NOT NULL COMMENT '收藏商品ID',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  INDEX `fk[group_collections]collect_group_id`(`collect_group_id`) USING BTREE,
+  CONSTRAINT `fk[group_collections]collect_group_id` FOREIGN KEY (`collect_group_id`) REFERENCES `product_group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk[group_collections]user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of group_collections
+-- ----------------------------
+INSERT INTO `group_collections` VALUES (1, 1);
 
 -- ----------------------------
 -- Table structure for order_detail
@@ -249,6 +267,24 @@ INSERT INTO `store` VALUES (13, '男人帮', '该品牌创立于yyyy年', 'https
 INSERT INTO `store` VALUES (14, '好运来', '该品牌创立于yyyy年', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2135299834,1746253257&fm=26&gp=0.jpg', 2, '2018-12-05 18:57:37');
 
 -- ----------------------------
+-- Table structure for store_collections
+-- ----------------------------
+DROP TABLE IF EXISTS `store_collections`;
+CREATE TABLE `store_collections`  (
+  `user_id` int(55) NOT NULL COMMENT '用户ID',
+  `collect_store_id` int(55) NOT NULL COMMENT '收藏店铺ID',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  INDEX `fk[store_collections]collect_store_id`(`collect_store_id`) USING BTREE,
+  CONSTRAINT `fk[store_collections]collect_store_id` FOREIGN KEY (`collect_store_id`) REFERENCES `store` (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk[store_collections]user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of store_collections
+-- ----------------------------
+INSERT INTO `store_collections` VALUES (1, 2);
+
+-- ----------------------------
 -- Table structure for store_product
 -- ----------------------------
 DROP TABLE IF EXISTS `store_product`;
@@ -290,22 +326,6 @@ CREATE TABLE `user`  (
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'username', '$2a$10$F8DJhuyZF.xt2elWxkZ0TezCjjQQA.DcOLReN9bGvc80Zu88OnGca', 'Tom', b'1', '11122223333', '中国矿业大学松苑', '2018-12-05 18:50:00', '中国矿业大学');
 INSERT INTO `user` VALUES (2, 'maker', '$2a$10$sT0tVlaWN/rZ5feB1jtRCef0FJLB0dDmK2c6ZmZONYhiAx35UzR3K', 'Jason', b'1', '44455556666', '中国矿业大学桃苑', '2018-12-05 18:47:15', '中国矿业大学（北京）');
-
--- ----------------------------
--- Table structure for user_collections
--- ----------------------------
-DROP TABLE IF EXISTS `user_collections`;
-CREATE TABLE `user_collections`  (
-  `user_id` int(55) NOT NULL COMMENT '用户ID',
-  `collect_group_id` int(55) NOT NULL COMMENT '收藏商品ID',
-  `collect_store_id` int(55) NULL DEFAULT NULL COMMENT '收藏店铺ID',
-  PRIMARY KEY (`user_id`) USING BTREE,
-  INDEX `fk[user_collections]collect_group_id`(`collect_group_id`) USING BTREE,
-  INDEX `fk[user_collections]collect_store_id`(`collect_store_id`) USING BTREE,
-  CONSTRAINT `fk[user_collections]collect_group_id` FOREIGN KEY (`collect_group_id`) REFERENCES `product_group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk[user_collections]collect_store_id` FOREIGN KEY (`collect_store_id`) REFERENCES `store` (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk[user_collections]user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_role
